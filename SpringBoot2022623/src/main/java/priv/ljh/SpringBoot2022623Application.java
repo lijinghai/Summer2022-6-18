d69245af3dc650ce209cec30d97eb69f1e2818f3
lijinghai 台式机 <lijinghailjh@163.com>
2022-06-23 18:59:23 +0800

    :tada:

---
+++ SpringBoot2022623/src/main/java/priv/ljh/SpringBoot2022623Application.java
@@ -0,0 +1,13 @@
+package priv.ljh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2022623Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2022623Application.class, args);
+    }
+
+}
---
+++ SpringBoot2022623/src/test/java/priv/ljh/SpringBoot2022623ApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.ljh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2022623ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
