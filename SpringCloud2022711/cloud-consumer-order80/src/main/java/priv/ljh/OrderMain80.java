1a0395457568fc12d41ba79b2c5dfbc1e50c1fd3
lijinghai 台式机 <lijinghailjh@163.com>
2022-07-12 17:09:22 +0800

    :sparkles: 订单微服务80入驻进EurekaServer

--- SpringCloud2022711/cloud-consumer-order80/src/main/java/priv/ljh/OrderMain80.java
+++ SpringCloud2022711/cloud-consumer-order80/src/main/java/priv/ljh/OrderMain80.java
@@ -2,6 +2,7 @@ package priv.ljh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author lijinghai
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Version: v1.0
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
