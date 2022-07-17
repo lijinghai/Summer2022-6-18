dd6e5b12e50556c94791b2179695858bb18e347a
lijinghai 台式机 <lijinghailjh@163.com>
2022-07-17 21:04:55 +0800

    :zap: Hystrix之服务降级支付侧fallback

--- SpringCloud2022711/cloud-provider-hygtrix-mypayment8001/src/main/java/priv/ljh/service/PaymentService.java
+++ SpringCloud2022711/cloud-provider-hygtrix-mypayment8001/src/main/java/priv/ljh/service/PaymentService.java
@@ -1,5 +1,6 @@
 package priv.ljh.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -17,9 +18,17 @@ public class PaymentService {
         return "线程池:  "+Thread.currentThread().getName()+"  paymentInfo_OK,id:  "+id+"\t"+"O(∩_∩)O哈哈~";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler")
     public String paymentInfo_TimeOut(Integer id)
     {
-        try { TimeUnit.MILLISECONDS.sleep(3000); } catch (InterruptedException e) { e.printStackTrace(); }
+        int timeNumber = 5;
+        try { TimeUnit.MILLISECONDS.sleep(timeNumber); } catch (InterruptedException e) { e.printStackTrace(); }
         return "线程池:  "+Thread.currentThread().getName()+" id:  "+id+"\t"+"O(∩_∩)O哈哈~"+"  耗时(秒): 3";
     }
+
+    public String paymentInfo_TimeOutHandler(Integer id)
+    {
+        return "线程池:  "+Thread.currentThread().getName()+"  8001系统繁忙或者运行报错，请稍后再试,id:  "+id+"\t"+"o(╥﹏╥)o";
+    }
+
 }
