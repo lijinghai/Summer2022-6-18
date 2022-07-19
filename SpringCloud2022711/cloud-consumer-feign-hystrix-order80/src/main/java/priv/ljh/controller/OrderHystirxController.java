d1b1d75b1af7757feb1024d95da356df16b3229c
lijinghai 台式机 <lijinghailjh@163.com>
2022-07-19 14:12:32 +0800

    :bug:  Hystrix之全局服务降级DefaultProperties

--- SpringCloud2022711/cloud-consumer-feign-hystrix-order80/src/main/java/priv/ljh/controller/OrderHystirxController.java
+++ SpringCloud2022711/cloud-consumer-feign-hystrix-order80/src/main/java/priv/ljh/controller/OrderHystirxController.java
@@ -1,5 +1,6 @@
 package priv.ljh.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,8 @@ import javax.annotation.Resource;
 
 @RestController
 @Slf4j
+// 全局回调的方法
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
 public class OrderHystirxController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
@@ -30,9 +33,12 @@ public class OrderHystirxController {
 //    }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
-            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="1500")
-    })
+    // 使用指定的
+//    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
+//            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="1500")
+//    })
+    //用全局的fallback方法
+    @HystrixCommand
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
 //        int age = 10/0;
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
@@ -42,6 +48,13 @@ public class OrderHystirxController {
     //善后方法
     public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id){
         return "我是消费者80,对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
+    }
+
+
+    // 下面是全局fallback方法
+    public String payment_Global_FallbackMethod()
+    {
+        return "Global异常处理信息，请稍后再试，/(ㄒoㄒ)/~~";
     }
 
 }
