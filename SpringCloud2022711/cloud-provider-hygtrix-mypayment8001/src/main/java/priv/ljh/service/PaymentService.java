02a239d10e9bc886c65201b1df6d555a3289aa1c
lijinghai 台式机 <lijinghailjh@163.com>
2022-07-19 14:57:57 +0800

    :sparkles: Hystrix之服务熔断

--- SpringCloud2022711/cloud-provider-hygtrix-mypayment8001/src/main/java/priv/ljh/service/PaymentService.java
+++ SpringCloud2022711/cloud-provider-hygtrix-mypayment8001/src/main/java/priv/ljh/service/PaymentService.java
@@ -47,6 +47,7 @@ public class PaymentService {
         if(id < 0) {
             throw new RuntimeException("******id 不能负数");
         }
+        // hutool
         String serialNumber = IdUtil.simpleUUID();
 
         return Thread.currentThread().getName()+"\t"+"调用成功，流水号: " + serialNumber;
