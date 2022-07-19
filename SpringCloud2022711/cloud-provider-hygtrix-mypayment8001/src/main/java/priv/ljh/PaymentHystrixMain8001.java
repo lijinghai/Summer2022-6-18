6a03c4ae273e8e2c2c41ba26c8037a7388277ee3
lijinghai 台式机 <lijinghailjh@163.com>
2022-07-19 13:54:52 +0800

    :bug:  hystrix fallback 超时回调

--- SpringCloud2022711/cloud-provider-hygtrix-mypayment8001/src/main/java/priv/ljh/PaymentHystrixMain8001.java
+++ SpringCloud2022711/cloud-provider-hygtrix-mypayment8001/src/main/java/priv/ljh/PaymentHystrixMain8001.java
@@ -3,12 +3,14 @@ package priv.ljh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class PaymentHystrixMain8001
 {
     public static void main(String[] args) {
--- SpringCloud2022711/cloud-provider-hygtrix-mypayment8001/src/main/java/priv/ljh/service/PaymentService.java
+++ SpringCloud2022711/cloud-provider-hygtrix-mypayment8001/src/main/java/priv/ljh/service/PaymentService.java
@@ -1,6 +1,7 @@
 package priv.ljh.service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -18,14 +19,15 @@ public class PaymentService {
         return "线程池:  "+Thread.currentThread().getName()+"  paymentInfo_OK,id:  "+id+"\t"+"O(∩_∩)O哈哈~";
     }
 
-    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler")
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler"/*指定善后方法名*/,commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="5000")
+    })
     public String paymentInfo_TimeOut(Integer id)
     {
-        int timeNumber = 5;
-        try { TimeUnit.MILLISECONDS.sleep(timeNumber); } catch (InterruptedException e) { e.printStackTrace(); }
-        return "线程池:  "+Thread.currentThread().getName()+" id:  "+id+"\t"+"O(∩_∩)O哈哈~"+"  耗时(秒): 3";
+//        int timeNumber = 5;
+        try { TimeUnit.MILLISECONDS.sleep(13000); } catch (InterruptedException e) { e.printStackTrace(); }
+        return "线程池:  "+Thread.currentThread().getName()+" id:  "+id+"\t"+"O(∩_∩)O哈哈~"+"  耗时(秒): " ;
     }
-
     public String paymentInfo_TimeOutHandler(Integer id)
     {
         return "线程池:  "+Thread.currentThread().getName()+"  8001系统繁忙或者运行报错，请稍后再试,id:  "+id+"\t"+"o(╥﹏╥)o";
