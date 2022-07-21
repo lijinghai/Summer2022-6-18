4fd3d05bc4d2c1a23458cdae5ab4e7ad82ae110e
lijinghai 台式机 <lijinghailjh@163.com>
2022-07-21 12:22:18 +0800

    :sparkles: Nacos之服务配置中心

--- SpringCloud2022711/cloudalibaba-config-nacos-client3377/src/main/java/priv/ljh/NacosConfigClientMain3377.java
+++ SpringCloud2022711/cloudalibaba-config-nacos-client3377/src/main/java/priv/ljh/NacosConfigClientMain3377.java
@@ -1,23 +1,21 @@
 package priv.ljh;
 
-import com.sun.org.apache.xpath.internal.operations.Or;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author lijinghai
- * @ClassName:OrderNacosMain83
+ * @ClassName:NacosConfigClientMain3377
  * @Email: lijinghailjh@163.com
  * @Description:
- * @Date: 2022/7/21 11:04
+ * @Date: 2022/7/21 11:49
  * @Version: v1.0
  */
-
 @SpringBootApplication
 @EnableDiscoveryClient
-public class OrderNacosMain83 {
+public class NacosConfigClientMain3377 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderNacosMain83.class,args);
+        SpringApplication.run(NacosConfigClientMain3377.class,args);
     }
 }
