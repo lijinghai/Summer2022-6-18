ed4abf57df21a660eb6424d2683ec5ebd7fe75df
lijinghai 台式机 <lijinghailjh@163.com>
2022-07-21 11:06:38 +0800

    :sparkles: Nacos之服务消费者注册83

--- SpringCloud2022711/cloudalibaba-consumer-nacos-order83/src/main/java/priv/ljh/OrderNacosMain83.java
+++ SpringCloud2022711/cloudalibaba-consumer-nacos-order83/src/main/java/priv/ljh/OrderNacosMain83.java
@@ -1,21 +1,21 @@
 package priv.ljh;
 
+import com.sun.org.apache.xpath.internal.operations.Or;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author lijinghai
- * @ClassName:PayMentMain9001
+ * @ClassName:OrderNacosMain83
  * @Email: lijinghailjh@163.com
- * @Description: alibaba服务提供者
- * @Date: 2022/7/21 10:49
+ * @Description:
+ * @Date: 2022/7/21 11:04
  * @Version: v1.0
  */
+
 @SpringBootApplication
-@EnableDiscoveryClient
-public class PayMentMain9001 {
+public class OrderNacosMain83 {
     public static void main(String[] args) {
-        SpringApplication.run(PayMentMain9001.class,args);
+        SpringApplication.run(OrderNacosMain83.class,args);
     }
 }
---
+++ SpringCloud2022711/cloudalibaba-provider-payment9001/src/main/java/priv/ljh/PaymentMain9001.java
@@ -0,0 +1,21 @@
+package priv.ljh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author lijinghai
+ * @ClassName:PayMentMain9001
+ * @Email: lijinghailjh@163.com
+ * @Description: alibaba服务提供者
+ * @Date: 2022/7/21 10:49
+ * @Version: v1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class,args);
+    }
+}
