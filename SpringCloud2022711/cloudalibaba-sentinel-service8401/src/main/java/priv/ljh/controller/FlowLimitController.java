42db5aaeabd761458d2cf36e0126d963b9974901
lijinghai 台式机 <lijinghailjh@163.com>
2022-07-23 11:00:16 +0800

    :sparkles:  Sentinel 热点限流

--- SpringCloud2022711/cloudalibaba-sentinel-service8401/src/main/java/priv/ljh/controller/FlowLimitController.java
+++ SpringCloud2022711/cloudalibaba-sentinel-service8401/src/main/java/priv/ljh/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package priv.ljh.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -48,6 +51,20 @@ public class FlowLimitController {
         int a = 10/0;
         return "----testD";
     }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler/*兜底方法*/ = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2) {
+        //int age = 10/0;
+        return "------testHotKey";
+    }
+
+    /*兜底方法*/
+    public String deal_testHotKey (String p1, String p2, BlockException exception) {
+        return "------deal_testHotKey,o(╥﹏╥)o";  //sentinel系统默认的提示：Blocked by Sentinel (flow limiting)
+    }
+
 
 }
 
