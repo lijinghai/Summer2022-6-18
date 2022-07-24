d34f2a165f71aebfe87367b6158e453c0f57a7cf
lijinghai 台式机 <lijinghailjh@163.com>
2022-07-24 09:29:40 +0800

    :sparkles:  Seata

---
+++ SpringCloud2022711/web-test1015/src/main/java/priv/ljh/WebTestMain1015.java
@@ -0,0 +1,20 @@
+package priv.ljh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsProperties;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author lijinghai
+ * @ClassName:WebTestMain1015
+ * @Email: lijinghailjh@163.com
+ * @Description:
+ * @Date: 2022/7/23 18:14
+ * @Version: v1.0
+ */
+@SpringBootApplication
+public class WebTestMain1015 {
+    public static void main(String[] args) {
+        SpringApplication.run(WebTestMain1015.class,args);
+    }
+}
