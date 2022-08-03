5f003cbfdc54174d775b11a4081bc9d1fff501fc
lijinghai 台式机 <lijinghailjh@163.com>
2022-08-03 19:44:01 +0800

    :sparkles: 引入getway

--- mall/mall-getway/src/main/java/priv/ljh/MallGetwayApplication.java
+++ mall/mall-getway/src/main/java/priv/ljh/MallGetwayApplication.java
@@ -1,4 +1,4 @@
-package priv.ljh.mall.order;
+package priv.ljh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class MallOrderApplication {
+public class MallGetwayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MallOrderApplication.class, args);
+        SpringApplication.run(MallGetwayApplication.class, args);
     }
 
 }
---
+++ mall/mall-getway/src/test/java/priv/ljh/MallGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.ljh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallGetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
